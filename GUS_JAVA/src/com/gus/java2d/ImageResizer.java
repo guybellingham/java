6f01d90837bcf4444339bbbb1da50456bcd0c72e
guybellingham <gbellingham@innotas.com>
2016-02-26 17:03:04 -0800

    New highQuality image scaling

--- GUS_JAVA/src/com/gus/java2d/ImageResizer.java
+++ GUS_JAVA/src/com/gus/java2d/ImageResizer.java
@@ -1,18 +1,44 @@
 package com.gus.java2d;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageInputStream;
+
+import com.gus.java2d.CropMethod;
 
 public class ImageResizer {
 
+	/**
+	 * Method uses Java2D Bilinear sampling to scale a (jpeg) image to the given 
+	 * <code>width</code> and <code>height</code>. Does not necessarily maintain 
+	 * the image aspect ratio, that is up to the caller whose responsibility it is 
+	 * to pick the 'correct' width and height or to crop the image first. 
+	 * @param image - input image buffer
+	 * @param width
+	 * @param height
+	 * @return output (scaled) image buffer
+	 * @throws IOException
+	 */
 	public static BufferedImage getScaledImage(BufferedImage image, int width, int height) throws IOException {
 	    int imageWidth  = image.getWidth();
 	    int imageHeight = image.getHeight();
@@ -21,21 +47,30 @@ public class ImageResizer {
 	    double scaleY = (double)height/imageHeight;
 	    AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleX, scaleY);
 	    AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
-
+	    
 	    return bilinearScaleOp.filter(
 	        image,
 	        new BufferedImage(width, height, image.getType()));
 	}
-	
+	/**
+	 * Crops a given image to the given <code>width</code> and <code>height</code>. 
+	 * Note: The (new) width and height must be less than the existing images width and height! 
+	 * @param image
+	 * @param width
+	 * @param height
+	 * @param method - {@link CropMethod} controls how the cropping square or rectangle is positioned (offset). 
+	 * @return the cropped image buffer
+	 * @throws IOException
+	 */
 	public static BufferedImage getCroppedImage(BufferedImage image, int width, int height, CropMethod method) throws IOException {
 		assert(image != null);
 		int imageWidth  = image.getWidth();
 	    int imageHeight = image.getHeight();
-	    if(width >= imageWidth) {
-			throw new IOException("Cropped width "+width+" must be less than image width "+imageWidth+"!");
+	    if(width > imageWidth) {
+			throw new IOException("Cropped width "+width+" cannot be greater than image width "+imageWidth+"!");
 		}
-	    if(height >= imageHeight) {
-			throw new IOException("Cropped height "+height+" must be less than image height "+imageHeight+"!");
+	    if(height > imageHeight) {
+			throw new IOException("Cropped height "+height+" cannot be greater than image height "+imageHeight+"!");
 		}
 	    int x = 0 , y = 0;   //coords to crop from
 
@@ -58,7 +93,15 @@ public class ImageResizer {
 		
 	}
 	
-	/*
+	public static BufferedImage getScaledImageHighQuality(BufferedImage image, int width, int height) throws IOException {
+		int imageWidth  = image.getWidth();
+	    int imageHeight = image.getHeight();
+
+	    double scaleX = (double)width/imageWidth;
+	    double scaleY = (double)height/imageHeight;
+	    
+		BufferedImage outputImage = new BufferedImage(width,height,image.getType());
+		
 		Graphics2D graphics2D = outputImage.createGraphics();
 		
 		AffineTransform savedAT = graphics2D.getTransform();
@@ -90,11 +133,71 @@ public class ImageResizer {
 			}
 			
 		}
-		
 		graphics2D.setTransform(savedAT);
 		graphics2D.dispose();
 		return outputImage;
-	*/
+	}
+	
+
+	/** 
+	 * https://community.oracle.com/docs/DOC-983611 
+	 * Convenience method that returns a scaled instance of the 
+	 * provided {@code BufferedImage}. 
+	 * @param img the original image to be scaled 
+	 * @param targetWidth the desired width of the scaled instance, * in pixels * 
+	 * @param targetHeight the desired height of the scaled instance, * in pixels * 
+	 * @param hint one of the rendering hints that corresponds to 
+	 *  {@code RenderingHints.KEY_INTERPOLATION} (e.g. 
+	 *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR}, 
+	 *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR}, 
+	 *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC}
+	 *  ) 
+	 * @param higherQuality if true, this method will use a multi-step  
+	 * scaling technique that provides higher quality than the usual 
+	 * one-step technique (only useful in downscaling cases, where 
+	 * {@code targetWidth} or {@code targetHeight} is 
+	 * smaller than the original dimensions, and generally only when 
+	 * the {@code BILINEAR} hint is specified) 
+	 * @return a scaled version of the original {@code BufferedImage} 
+	 */ 
+	public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint, boolean higherQuality) { 
+		int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB; 
+		BufferedImage ret = (BufferedImage)img; 
+		int w, h; 
+		if (higherQuality) { 
+			// Use multi-step technique: start with original size, then 
+			// scale down in multiple passes with drawImage() 
+			// until the target size is reached 
+			w = img.getWidth(); h = img.getHeight(); 
+		} else { 
+			// Use one-step technique: scale directly from original size 
+			// to target size with a single drawImage() call 
+			w = targetWidth; h = targetHeight; 
+		} 
+		do { 
+			if (higherQuality && w > targetWidth) { 
+				w /= 2; 
+				if (w < targetWidth) { 
+					w = targetWidth; 
+				} 
+			} 
+			if (higherQuality && h > targetHeight) { 
+				h /= 2; 
+				if (h < targetHeight) {
+					h = targetHeight; 
+					} 
+				} 
+			BufferedImage tmp = new BufferedImage(w, h, type); 
+			Graphics2D g2 = tmp.createGraphics(); 
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint); 
+			// use the scaling variant of Graphics.drawImage() - no AffineTransform needed!
+			g2.drawImage(ret, 0, 0, w, h, null); 
+			
+			g2.dispose(); 
+			ret = tmp; 
+		} while (w != targetWidth || h != targetHeight); 
+		return ret; 
+	}
 	
 	public static class ThumbnailReadyObserver implements ImageObserver {
 
@@ -128,16 +231,56 @@ public class ImageResizer {
 		BufferedImage buffer = ImageIO.read(imageBytes);
 		return buffer;
 	}
+	public static BufferedImage getImage(InputStream imageBytes, String mimeType) throws IOException {
+		BufferedInputStream bis = new BufferedInputStream(imageBytes);
+		BufferedImage image = null;
+		Iterator<ImageReader> it = ImageIO.getImageReadersByMIMEType(mimeType);
+		ImageReader reader = it.next();
+		ImageInputStream iis = ImageIO.createImageInputStream(bis);
+		reader.setInput(iis, false, false);
+		image = reader.read(0);
+		int width = image.getWidth(null);
+		int height = image.getHeight(null);
+		System.out.println("getImage type="+mimeType+" got buffered image from stream width="+width+" height="+height);
+		IIOMetadata imageMetadata = reader.getImageMetadata(0);
+		return image;
+	}
 	
-	public static boolean writeBufferedImage(BufferedImage buffer, File outputFile) {
+	public static boolean writeBufferedImage(BufferedImage buffer, String format, File outputFile) {
 		try {
-			return ImageIO.write(buffer, "jpg", outputFile);
+			return ImageIO.write(buffer, format, outputFile);
 		} catch (IOException e) {
 			System.err.println("FAILED to write buffer to output file '"+outputFile+"'!");
 			return false;
 		}
 	}
 	
+	/**
+	 * Write a JPEG file setting the compression quality.
+	 * 
+	 * @param image -  a BufferedImage to be saved
+	 * @param outputFile - destination .JPG file 
+	 * @param quality -  a decimal between 0.0 and 1.0, where 1.0 means uncompressed.
+	 * @throws IOException  in case of problems writing the file
+	 */
+	public static void writeJpeg(BufferedImage image, File outputFile, float quality) 
+	throws IOException {
+	    ImageWriter writer = null;
+	    FileImageOutputStream output = null;
+	    try {
+	        writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+	        ImageWriteParam param = writer.getDefaultWriteParam();
+	        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+	        param.setCompressionQuality(quality);
+	        output = new FileImageOutputStream(outputFile);
+	        writer.setOutput(output);
+	        IIOImage iioImage = new IIOImage(image, null, null);
+	        writer.write(null, iioImage, param);
+	    } finally {
+	        if (writer != null) writer.dispose();
+	        if (output != null) output.close();
+	    }
+	}
     /**
      * Get a List of accepted File Types.
      * Normally, these are: bmp, jpg, wbmp, jpeg, png, gif
--- GUS_JAVA/src/com/gus/java2d/ImageResizerTest.java
+++ GUS_JAVA/src/com/gus/java2d/ImageResizerTest.java
@@ -2,6 +2,7 @@ package com.gus.java2d;
 
 import static org.junit.Assert.fail;
 
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,24 +14,177 @@ import com.gus.java2d.CropMethod;
 
 public class ImageResizerTest {
 
-	protected InputStream getInputImageStream() {
-		InputStream clouds = this.getClass().getClassLoader().getResourceAsStream("com/gus/java2d/clouds.jpg");
+	public static final String FORMAT_JPEG = "jpg";
+	public static final String MIME_TYPE_JPEG = "image/jpeg";
+	public static final String FORMAT_GIF = "gif";
+	public static final String MIME_TYPE_GIF = "image/gif";
+	public static final String FORMAT_PNG = "png";
+	public static final String MIME_TYPE_PNG = "image/png";
+	
+	protected InputStream getInputImageStream(String filepath) {
+		InputStream clouds = this.getClass().getClassLoader().getResourceAsStream(filepath);
 		return clouds;
 	}
 	
 	@Test
-	public void testScaleImage() {
-		
+	public void testScaleJPEGImage() {
 		try {
-			File outputFile = new File("C:\\Temp\\clouds_scaled.jpg");
+			File outputFile = new File("C:\\Temp\\joker_scaled_60.jpg");
 			if(outputFile.exists()) {
 				outputFile.delete();
 			}
-			InputStream clouds = getInputImageStream();
+			InputStream joker = getInputImageStream("com/gus/java2d/joker-2048.jpg");
+			if(joker != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(joker); 
+				BufferedImage scaledBuffer = ImageResizer.getScaledImage(buffer, 60, 60);
+				ImageResizer.writeBufferedImage(scaledBuffer, FORMAT_JPEG, outputFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testScaleJPEGImageBilinearHighQuality() {
+		try {
+			File outputFile = new File("C:\\Temp\\joker_scaled_bilinear_60_hq.jpg");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream joker = getInputImageStream("com/gus/java2d/joker-2048.jpg");
+			if(joker != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(joker); 
+				BufferedImage scaledBuffer = ImageResizer.getScaledInstance(buffer, 60, 60, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
+				ImageResizer.writeJpeg(scaledBuffer, outputFile, 0.95F);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testCropAndScaleJPEGImageThumbnail() {
+		try {
+			File outputFile = new File("C:\\Temp\\joker_crop_scaled_60.jpg");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream joker = getInputImageStream("com/gus/java2d/joker-2048.jpg");
+			if(joker != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(joker); 
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 1366, 1366, CropMethod.TOP_CENTER);
+				BufferedImage scaledBuffer = ImageResizer.getScaledImage(croppedBuffer, 60, 60);
+				ImageResizer.writeBufferedImage(scaledBuffer, FORMAT_JPEG, outputFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testCropAndScaleJPEGHighQuality() {
+		try {
+			File outputFile = new File("C:\\Temp\\joker_crop_scaled_bicubic_60_hq.jpg");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream joker = getInputImageStream("com/gus/java2d/joker-2048.jpg");
+			if(joker != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(joker); 
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 1366, 1366, CropMethod.TOP_CENTER);
+				BufferedImage scaledBuffer = ImageResizer.getScaledInstance(croppedBuffer, 60, 60, RenderingHints.VALUE_INTERPOLATION_BICUBIC, true);
+				ImageResizer.writeJpeg(scaledBuffer, outputFile, 1.0F);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testCropAndScaleLargeGreyJPEGImage() {
+		try {
+			File outputFile = new File("C:\\Temp\\headshot_crop_scaled_bicubic_60_hq.jpg");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			System.out.println("Reading greyscale.jpg");
+			long start = System.currentTimeMillis(), stop = 0L; 
+			InputStream greyscaled = getInputImageStream("com/gus/java2d/headshot.jpg");
+			if(greyscaled != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(greyscaled); 
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 2187, 2187, CropMethod.TOP_CENTER);
+				BufferedImage scaledBuffer = ImageResizer.getScaledInstance(croppedBuffer, 60, 60, RenderingHints.VALUE_INTERPOLATION_BICUBIC, true);
+				ImageResizer.writeJpeg(scaledBuffer, outputFile, 0.9F);
+				stop = System.currentTimeMillis();
+				System.out.println("Wrote grey_scaled.jpg in "+(stop - start)+"ms");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testScaleGIFImage() {
+		try {
+			File outputFile = new File("C:\\Temp\\homer_scaled_60_hq.gif");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream clouds = getInputImageStream("com/gus/java2d/homer.gif");
 			if(clouds != null) {
 				BufferedImage buffer =  ImageResizer.getBufferedImage(clouds); 
-				BufferedImage scaledBuffer = ImageResizer.getScaledImage(buffer, 100, 100);
-				ImageResizer.writeBufferedImage(scaledBuffer, outputFile);
+				BufferedImage scaledBuffer = ImageResizer.getScaledInstance(buffer, 60, 60, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
+				ImageResizer.writeBufferedImage(scaledBuffer, FORMAT_GIF, outputFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testScalePNGImage() {
+		try {
+			File outputFile = new File("C:\\Temp\\lisa_scaled_60_hq.png");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream clouds = getInputImageStream("com/gus/java2d/Lisa.png");
+			if(clouds != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(clouds); 
+				BufferedImage scaledBuffer = ImageResizer.getScaledInstance(buffer, 60, 60, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
+				ImageResizer.writeBufferedImage(scaledBuffer, FORMAT_PNG, outputFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Test
+	public void testCropJPEGImageTopCenter() {
+		try {
+			File outputFile = new File("C:\\Temp\\bart_crop_topcenter.jpg");
+			if(outputFile.exists()) {
+				outputFile.delete();
+			}
+			InputStream bart = getInputImageStream("com/gus/java2d/bart.jpg");
+			if(bart != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(bart); 
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 300, 300, CropMethod.TOP_CENTER);
+				ImageResizer.writeBufferedImage(croppedBuffer, FORMAT_JPEG, outputFile);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -39,19 +193,18 @@ public class ImageResizerTest {
 		}
 		
 	}
-
 	@Test
-	public void testCropImageTopCenter() {
+	public void testCropGIFImageCenterCenter() {
 		try {
-			File outputFile = new File("C:\\Temp\\clouds_crop_topcenter.jpg");
+			File outputFile = new File("C:\\Temp\\mrburns_crop_centercenter.gif");
 			if(outputFile.exists()) {
 				outputFile.delete();
 			}
-			InputStream clouds = getInputImageStream();
-			if(clouds != null) {
-				BufferedImage buffer =  ImageResizer.getBufferedImage(clouds); 
-				BufferedImage scaledBuffer = ImageResizer.getCroppedImage(buffer, 100, 100, CropMethod.TOP_CENTER);
-				ImageResizer.writeBufferedImage(scaledBuffer, outputFile);
+			InputStream mrburns = getInputImageStream("com/gus/java2d/mrburns.gif");
+			if(mrburns != null) {
+				BufferedImage buffer =  ImageResizer.getBufferedImage(mrburns); 
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 200, 200, CropMethod.CENTER_CENTER);
+				ImageResizer.writeBufferedImage(croppedBuffer,  FORMAT_GIF, outputFile);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -61,17 +214,17 @@ public class ImageResizerTest {
 		
 	}
 	@Test
-	public void testCropImageCenterCenter() {
+	public void testCropPNGImageTopCenter() {
 		try {
-			File outputFile = new File("C:\\Temp\\clouds_crop_centercenter.jpg");
+			File outputFile = new File("C:\\Temp\\lisa_crop_topcenter.png");
 			if(outputFile.exists()) {
 				outputFile.delete();
 			}
-			InputStream clouds = getInputImageStream();
+			InputStream clouds = getInputImageStream("com/gus/java2d/Lisa.png");
 			if(clouds != null) {
 				BufferedImage buffer =  ImageResizer.getBufferedImage(clouds); 
-				BufferedImage scaledBuffer = ImageResizer.getCroppedImage(buffer, 100, 100, CropMethod.CENTER_CENTER);
-				ImageResizer.writeBufferedImage(scaledBuffer, outputFile);
+				BufferedImage croppedBuffer = ImageResizer.getCroppedImage(buffer, 400, 400, CropMethod.TOP_CENTER);
+				ImageResizer.writeBufferedImage(croppedBuffer,  FORMAT_PNG, outputFile);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
