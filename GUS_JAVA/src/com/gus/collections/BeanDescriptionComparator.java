118eaae6352f90f6ef733adec4f45cfb830a31aa
guybellingham <gbellingham@innotas.com>
2018-11-28 13:47:09 -0800

    Improve Comparator.comparing() example

--- GUS_JAVA/src/com/gus/collections/BeanDescriptionComparator.java
+++ GUS_JAVA/src/com/gus/collections/BeanDescriptionComparator.java
@@ -6,10 +6,11 @@ public class BeanDescriptionComparator implements Comparator<ComparableBean> {
 	/**
 	 * Compares its two arguments for order. Returns a negative integer, zero, or a positive integer 
 	 * as the first argument is less than, equal to, or greater than the second.
+	 * <li>Sorts <code>description</code> in 'reverse' (descending) order
 	 */
 	@Override
 	public int compare(ComparableBean o1, ComparableBean o2) {
-		return o1.getDescription().compareToIgnoreCase(o2.getDescription());
+		return o2.getDescription().compareToIgnoreCase(o1.getDescription());
 	}
 
 }
--- GUS_JAVA/src/com/gus/collections/ComparingApp.java
+++ GUS_JAVA/src/com/gus/collections/ComparingApp.java
@@ -11,22 +11,23 @@ public class ComparingApp {
 			new ComparableBean(1, "January"),
 			new ComparableBean(2, "February"),
 			new ComparableBean(3, "March"),
-			new ComparableBean(4, "April")
+			new ComparableBean(4, "April"),
+			new ComparableBean(5, "May"),
+			new ComparableBean(6, "June"),
+			new ComparableBean(7, "July"),
 		};
 	private static List<ComparableBean> MONTHS =  Arrays.asList(ARRAY);
 	
 	public static void main(String[] args) {
-		System.out.println("Sorting months by their names ...");
-		Collections.sort(MONTHS, Comparator.comparing((ComparableBean bean) -> bean.getDescription()));
+		System.out.println("Sorting months by their names using Comparator.comparing(Function)...");
+		Collections.sort(MONTHS, Comparator.comparing(ComparableBean::getDescription));
 		MONTHS.forEach(System.out::println);
 		
-		System.out.println("Sorting months by their natural order (id) ...");
-		Collections.sort(MONTHS);
-		MONTHS.forEach(System.out::println);
+		System.out.println("Sorting months by their natural order (by id) in a stream().sorted() ...");
+		MONTHS.stream().sorted().forEach(System.out::println);
 		
 		System.out.println("Sorting months using BeanDescriptionComparator ...");
-		Collections.sort(MONTHS, new BeanDescriptionComparator());
-		MONTHS.forEach(System.out::println);
+		MONTHS.stream().sorted(new BeanDescriptionComparator()).forEach(System.out::println);
 		
 	}
 
